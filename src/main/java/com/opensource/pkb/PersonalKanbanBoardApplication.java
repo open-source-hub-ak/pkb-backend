3b39e1f66e51f9d37f22ee5b1f99e8ac38a70ef5
aakash <aakash>
2021-02-18 17:54:29 +0530

    Added swagger configuration

--- src/main/java/com/opensource/pkb/PersonalKanbanBoardApplication.java
+++ src/main/java/com/opensource/pkb/PersonalKanbanBoardApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+
 public class PersonalKanbanBoardApplication {
 
 	@Bean
